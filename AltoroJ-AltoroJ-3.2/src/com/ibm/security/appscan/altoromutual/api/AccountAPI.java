8e5480c2d8bff5003ed7ebd41282724ac2da1690
ApporwaV <apporwa.v@hcl.com>
2019-08-16 00:46:32 -0700

    Change to trigger Jenkins

--- AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/AccountAPI.java
+++ AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/AccountAPI.java
@@ -23,7 +23,7 @@ import com.ibm.security.appscan.altoromutual.util.OperationsUtil;
 @Path("/account")
 public class AccountAPI extends AltoroAPI {
 
-	// For the get method return all accounts *these changes will be published*
+	// For the get method return all accounts *changes made to GIT*
 	@GET
 	public Response getAccounts(@Context HttpServletRequest request) {
 
