6ac1dfe87aab7ec8766902f8aa14d234ce6f9ca1
ApporwaV <apporwa.v@hcl.com>
2019-11-06 10:14:39 -0800

    commit changes - 2

--- AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/AccountAPI.java
+++ AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/AccountAPI.java
@@ -23,7 +23,7 @@ import com.ibm.security.appscan.altoromutual.util.OperationsUtil;
 @Path("/account")
 public class AccountAPI extends AltoroAPI {
 
-	// For the get method return all accounts *new changes --Jenkins 6th Nov -1 *
+	// For the get method return all accounts *new changes --Jenkins 6th Nov -2 *
 	@GET
 	public Response getAccounts(@Context HttpServletRequest request) {
 
