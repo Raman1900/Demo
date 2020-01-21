100031e38c1994a8d201a55319f8cc06de43df28
SVC <SVC@APPSEC-ENTERPRISE.CyberLab.com>
2020-01-21 03:28:46 -0800

    Commit-21Jan

--- AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/AccountAPI.java
+++ AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/AccountAPI.java
@@ -23,7 +23,7 @@ import com.ibm.security.appscan.altoromutual.util.OperationsUtil;
 @Path("/account")
 public class AccountAPI extends AltoroAPI {
 
-	// For the get method return all accounts *new changes --Jenkins 22nd Nov 2019 GV*
+	// For the get method return all accounts *new changes --Jenkins 21 Jan VS*
 	@GET
 	public Response getAccounts(@Context HttpServletRequest request) {
 
